d50676b0209761be13681a769dee24d960aadd65
Akhil Narang <akhilnarang.1999@gmail.com>
2019-09-22 20:19:49 +0530

    beryllium: set ro.build.fingerprint directly
    Required for ctsProfile/safetynet after https://github.com/AOSiP/platform_build/commit/9e73e82a12553529740c0f75ef4d72ac2e594e72
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- aosip_beryllium.mk
+++ aosip_beryllium.mk
@@ -24,3 +24,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="beryllium"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/V10.2.3.0.PEJMIXM:user/release-keys
